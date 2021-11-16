469d7d202dc700e86aefeef6d2b38ade235e1711
Omar228228 <goblock542@gmail.com>
2021-11-16 18:43:42 +0300

    first commit

---
+++ 6.2.1.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <conio.h>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void CreateArray(int* a, const int n, int Low, int High)
+{
+	for (int i = 0; i < n; i++) 
+	{
+		a[i] = Low + rand() % (High - Low + 1);
+	}
+}
+
+void PrintArray(int* a, const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << setw(4) << a[i];
+	}
+	cout << endl;
+}
+
+int Sum(int* a, const int n, int m, int S)
+{
+	for (int i = 0; i < n; i++) 
+	{
+		if (i % 2 == 1) {
+			m++;
+			S += a[i];
+		}
+	}
+	return S;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	const int n = 10;
+	int a[n];
+	int S;
+	int Low = 15;
+	int High = 85;
+
+	CreateArray(a, n, Low, High);
+	S = Sum(a, n, 0, 0);
+	PrintArray(a, n);
+	cout << "Medium Arifmetic: " << S / n << endl;
+}
---
+++ UnitTest6.2.1/UnitTest6.2.1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\Юра\source\repos\6.2.1\6.2.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest621
+{
+	TEST_CLASS(UnitTest621)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int n = 10;
+			int a[n] = { 29, 22, 25, 71, 26, 20, 20, 44, 71, 18 };
+			int res = (Sum(a, n, 0, 0) / n);
+			Assert::AreEqual(17, res);
+
+		}
+	};
+}
